3c179178d06527ee8f0dd11a5e63de9749a96097
Rajat Barman <rajat.barman@pharmeasy.in>
2018-12-27 02:10:02 +0530

    Using linux binary for pngquant

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const execa = require('execa');
 const isPng = require('is-png');
 const isStream = require('is-stream');
-const pngquant = require('pngquant-bin');
+const pngquant = require('./bin/linux/pngquant');
 
 module.exports = (options = {}) => input => {
 	const isBuffer = Buffer.isBuffer(input);
